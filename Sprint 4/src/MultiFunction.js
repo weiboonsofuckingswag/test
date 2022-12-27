9f36c130e0ea1f5ecca1e4e36cf52806071db881
weiboonsofuckingswag <76873597+weiboonsofuckingswag@users.noreply.github.com>
2022-12-27 09:36:55 +0800

    Update MultiFunction.js

--- Sprint 4/src/MultiFunction.js
+++ Sprint 4/src/MultiFunction.js
@@ -2,12 +2,16 @@ const { ethers } = require("hardhat");
 const { LoaderOptionsPlugin } = require("webpack");
 // For Hardhat
 const contract = require("../artifacts/contracts/mapping.sol/Mapping.json");
+const contract4 = require("../artifacts/contracts/firebase.sol/FirebaseSystem.json");
+
 // Provider
 const provider = new ethers.providers.WebSocketProvider("ws://localhost:9545");
 // Signer
 const signer = new ethers.Wallet("06b91f40bafe25bb97844ca576675d475ad4c197a4acab0f00a7dabf8326d480", provider);
 // Contract
 const payment = new ethers.Contract("0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0", contract.abi, signer);
+const firebase = new ethers.Contract("0xf204a4Ef082f5c04bB89F7D5E6568B796096735a", contract4.abi, signer);
+const rpadaily2 = require('./rpadaily2');
 
 
 
@@ -26,10 +30,13 @@ async function multiFunction() {
     console.log("1. Create client policy");
     console.log("2. Payment");
     console.log("3. Retrieve Policy Information");
-    const promptFunction = prompt("Enter the function (1-3) you wish to run: ");
+    console.log("4. Retrieve Payment Status with Policy ID")
+    const promptFunction = prompt("Enter the function (1-4) you wish to run: ");
 
     if (promptFunction == 1) {
         clientInfo.storebesu();
+                console.log(`The updated time is: ${rpadaily2.updatedtime}`);
+
     }
     else if (promptFunction == 2) {
         payment1.paymentrpa();
@@ -37,6 +44,19 @@ async function multiFunction() {
 
     else if (promptFunction == 3) {
         retrieve.retrieve();
+    }
+    else if (promptFunction ==4){
+        const policyDate = "2311";
+        const paymentStatus = 1;
+        const prompt = require('prompt-sync')();
+        const smartContractAddr = prompt("Enter Policy ID: ");
+        const viewId = await payment.getIdview(smartContractAddr);
+        const addresscheck = await firebase.checkForSpecificAddress(paymentStatus, policyDate, viewId);
+        console.log(smartContractAddr," payment status is: " ,paymentStatus);
+        console.log(`The updated time is: ${rpadaily2.updatedtime}`);
+
+
+            // console.log(smartContractAddr, " date of policy: ", date);
     }
     else {
         console.log("invalid option, try again.");
